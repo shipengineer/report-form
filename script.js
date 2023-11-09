4da9dc349617a0403e8ae986141d90d22fc2847b
MarineIngenier <poeticcrane@gmail.com>
2023-11-09 11:45:12 +0300

    semi-colon

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-// const allInputs = document.querySelectorAll('input');
-// allInputs.forEach((elem) => {
-//   elem.setAttribute('placeholder', '____');
-// });
+async function render() {
+  {
+    let responce = await fetch("./data.json");
+    let iData = responce.json();
+
+    const instrumentsDatalist = document.querySelector("#instruments");
+    console.log(iData);
+    const year = document.querySelector("#year");
+    year.textContent = iData.year;
+  }
+}
+
+const check_1 = document.querySelector("#full-measure");
+const check_2 = document.querySelector("#acceptance");
+const check_3 = document.querySelector("#to-correct-required");
+document.addEventListener("change", (e) => {
+  const target = e.target;
+  if (target.checked === true) {
+    target.parentNode.setAttribute("style", "text-decoration:none");
+  } else {
+    target.parentNode.setAttribute("style", "text-decoration:line-through 2px");
+  }
+});
+
+render();
