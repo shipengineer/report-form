ec375be21e425f7e4c745e0685dbdc2bf3537b9e
MarineIngenier <poeticcrane@gmail.com>
2023-11-13 09:07:53 +0300

    start 2nd page

--- script.js
+++ script.js
@@ -1,7 +1,60 @@
-async function render() {
+const instrumets = {
+  instruments: [
+    {
+      id: 1,
+      "instrument-name": "Лазерный трекер API Radian Plus R-80",
+      "instrument-number": "70681",
+      "instrument-certificate": "С-М/19-12-2022/211174153",
+    },
+    {
+      id: 2,
+      "instrument-name": "Сканер лазерный Leica RTC360",
+      "instrument-number": "2984274",
+      "instrument-certificate": "С-ДЭМ/19-09-2023/279399049",
+    },
+    {
+      id: 3,
+      "instrument-name": "Тахеометр Leica TS60 I",
+      "instrument-number": "890213",
+      "instrument-certificate": "С-Т/14-12-2022/208731450",
+    },
+    {
+      id: 4,
+      "instrument-name": "Тахеометр Sokkia NET1AXII",
+      "instrument-number": " КН0569",
+      "instrument-certificate": "С-ДЭМ/30-08-2023/274062460",
+    },
+    {
+      id: 5,
+      "instrument-name": "Тахеометр SOUTH N3",
+      "instrument-number": "287748",
+      "instrument-certificate": "С-ДЭМ/02-12-2022/205769401",
+    },
+    {
+      id: 6,
+      "instrument-name": "Нивелир Leica NA324 ",
+      "instrument-number": "941324337937",
+      "instrument-certificate": "С-ДЭМ/30-08-2023/274062459",
+    },
+    {
+      id: 7,
+      "instrument-name": "Лазерный дальномер Leica DISTO X3",
+      "instrument-number": "1610930131",
+      "instrument-certificate": "С-EBE/26-09-2022/188789417",
+    },
+    {
+      id: 8,
+      "instrument-name": "Рейка нивелирная RGK TS-5",
+      "instrument-number": "STS50003",
+      "instrument-certificate": "С-СП/21-03-2023/232461132",
+    },
+  ],
+  year: 2023,
+};
+function render() {
   {
-    let responce = await fetch("./data.json");
-    let iData = await responce.json();
+    // let responce = await fetch("./data.json");
+    let iData = instrumets;
 
     const instrumentsDatalist = document.querySelector("#instruments");
     const iInstruments = iData.instruments;
@@ -34,10 +87,7 @@ async function render() {
     });
   }
 }
-async function toChange() {
-  await render();
-}
-toChange();
+render();
 
 // _________________________________________________________________________
 const check_1 = document.querySelector("#full-measure");
@@ -62,3 +112,4 @@ const other = document.querySelector("#other");
 const otherText = document.querySelector("#other-text");
 
 // _________________________________________________________________________
+function addNewPage() {}
